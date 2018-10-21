f94be2b188c0b2cf75023886ca4ec9c454448c84
tax_k <taxfree9090@gmail.com>
2018-10-21 15:13:35 +0900

    Initial Commit

---
+++ cTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cTest
+//
+//  Created by tax_k on 21/10/2018.
+//  Copyright © 2018 tax_k. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
