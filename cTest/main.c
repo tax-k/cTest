b006fba10d91065bb124d29b706ca3149618315c
tax_k <taxfree9090@gmail.com>
2018-10-21 15:58:31 +0900

    C source from Xcode

--- cTest/main.c
+++ cTest/main.c
@@ -8,8 +8,35 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int n, array[1000], c, d, t;
+    
+    printf("입력 갯수: ");
+    scanf("%d", &n);
+    
+    printf("%d 개 입력\n", n);
+    
+    for (c = 0; c < n; c++)
+        scanf("%d", &array[c]);
+    
+    for (c = 1 ; c <= n - 1; c++) {
+        d = c;
+        
+        while ( d > 0 && array[d-1] > array[d]) {
+            t          = array[d];
+            array[d]   = array[d-1];
+            array[d-1] = t;
+            
+            d--;
+        }
+    }
+    
+    printf("정렬:\n");
+    
+    for (c = 0; c <= n - 1; c++) {
+        printf("%d ", array[c]);
+    }
+    
     return 0;
 }
